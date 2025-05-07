5b76ce61d678dbc3c51c07ae12bd8d20d7d52c16
hieuleminh03 <hieulm.data@gmail.com>
2025-05-08 00:25:45 +0700

    initial commit

---
+++ CreateImage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd ./BaseHadoop
+docker image build -t demo/hadoopha . --no-cache
+cd ..
+
+cd ./Workers
+docker image build -t demo/workers . --no-cache
+cd ..
+
+cd ./Spark
+docker image build -t demo/spark . --no-cache
+cd ..
+
+cd ./JournalNode1
+docker image build -t demo/journalnode1 . --no-cache
+cd ..
+
+cd ./JournalNode2
+docker image build -t demo/journalnode2 . --no-cache
+cd ..
+
+cd ./JournalNode3
+docker image build -t demo/journalnode3 . --no-cache
+cd ..
